625d3b4eb9281e388f79fac54be13bfaaa20224c
Matias Menendez <matias.95@hotmail.es>
2021-12-28 01:03:39 -0300

    Desafio 2 js

---
+++ clase2matiasmenendez.js
@@ -0,0 +1,32 @@
+let valor1 = Number(prompt("ingresar un numero entre 1 y 50"));
+
+let valor2 = Number(prompt("ingresar un numero entre 40 y 100"));
+
+if (valor1 > valor2){
+
+    alert("El numero " + valor1 + "es mayor a " + valor2);
+}
+else if (valor2 > valor1){
+
+    alert("El numero " + valor1 + "es menor a " + valor2);
+}
+else if (valor1 = valor2){
+
+    alert("El numero " + valor1 + "es igual a " + valor2);
+}
+else if (valor1 > 50){
+
+    alert("Uno de los valores ingresados es inocrrecto");
+}
+else if (1 > valor1){
+
+    alert("Uno de los valores ingresados es inocrrecto");
+}
+else if (valor2 > 100){
+
+    alert("Uno de los valores ingresados es inocrrecto");
+}
+else if (40 > valor2){
+
+    alert("Uno de los valore ingresado es inocrrecto");
+}
